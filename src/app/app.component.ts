54df3356c7d613ad73df3020625b3b8b55e26663
Alva Chien <alvachien@163.com>
2025-04-15 22:41:24 +0800

    New changes now can be print successfully

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,9 @@ import { QuestionBank, QuestionBankContentFormatEnum, QuestionBankItem, Question
   QuestionBankTypeKeys, 
   VALID_OPTION_KEYS, ValidOptionKeys } from './models/questionbank';
 import { NgxPrintModule } from 'ngx-print';
+import katex from 'katex';
+
+declare const renderMathInElement: any; // 声明 KaTeX 的渲染函数
 
 @Component({
   selector: 'app-root',
@@ -64,21 +67,54 @@ export class AppComponent implements OnInit, AfterViewInit {
       },
       {
         "id": "4",
-        "type": "FillInTheBlank",
-        "question": "《劝学》，作者：(@战国@)·@荀子@，原文：\n君子曰：@学不可以已@。\n@青，取之于蓝，而青于蓝@；@冰，水为之，而寒于水@。@木直中绳，𫐓以为轮，其曲中规@。@虽有槁暴，不复挺者，𫐓使之然也@。@故木受绳则直，金就砺则利，君子博学而日参省乎己，则知明而行无过矣@。@吾尝终日而思矣，不如须臾之所学也@；@吾尝跂而望矣，不如登高之博见也@。@登高而招，臂非加长也，而见者远@；@顺风而呼，声非加疾也，而闻者彰@。@假舆马者，非利足也，而致千里@；@假舟楫者，非能水也，而绝江河@。@君子生非异也，善假于物也@。",
+        "type": "Dictation",
+        "title": "《劝学》，作者：(战国)·荀子。",
+        "question": "君子曰：学不可以已。\n青，取之于蓝，而青于蓝；冰，水为之，而寒于水。木直中绳，𫐓以为轮，其曲中规。虽有槁暴，不复挺者，𫐓使之然也。故木受绳则直，金就砺则利，君子博学而日参省乎己，则知明而行无过矣。吾尝终日而思矣，不如须臾之所学也；吾尝跂而望矣，不如登高之博见也。登高而招，臂非加长也，而见者远；顺风而呼，声非加疾也，而闻者彰。假舆马者，非利足也，而致千里；假舟楫者，非能水也，而绝江河。君子生非异也，善假于物也。",
       },
       // {
       //   "id": "5",
       //   "type": "FillInTheBlank",
       //   "latex": true,
       //   "question": "0度角弧度数为@0@；30度角弧度数为@$\\frac{\\pi}{6}$@；45度角弧度数为@$\\frac{\\pi}{4}$@；60度角弧度数为@$\\frac{\\pi}{3}$@；90度角弧度数为@$\\frac{\\pi}{2}$@；120度角弧度数为@$\\frac{2\\pi}{3}$@；135度角弧度数为@$\\frac{3\\pi}{4}$@；150度角弧度数为@$\\frac{5\\pi}{6}$@；180度角弧度数为@$\\pi$@；210度角弧度数为@$\\frac{7\\pi}{6}$@；225度角弧度数为@$\\frac{5\\pi}{4}$@；240度角弧度数为@$\\frac{4\\pi}{3}$@；270度角弧度数为@$\\frac{3\\pi}{2}$@；300度角弧度数为@$\\frac{5\\pi}{3}$@；315度角弧度数为@$\\frac{7\\pi}{4}$@；330度角弧度数为@$\\frac{11\\pi}{6}$@；360度角弧度数为@$2\\pi$@。",
-      // }
-
-      // {
-      //     "type": "essay",
-      //     "question": "请写一篇关于人工智能的议论文",
-      //     "answer": "人工智能是一种通过模拟人类智能来实现任务的人工智能。它可以进行视觉识别、语音识别、语言理解和决策等任务。人工智能的发展已经取得了显著的进展，未来有望在各个领域发挥重要作用。"
-      // }
+      // },
+      {
+        "id": "6",
+        "type": "FillInTheBlank",
+        "latex": true,
+        "question": "已知$a^2+b^2=c^2$，则$a^2+b^2$的平方根为@$\\sqrt{a^2+b^2}=c$@。",
+      },
+      {
+        "id": "7",
+        "type": "FillInTheBlank",
+        "latex": true,
+        "question": "重要不等式：如果$a, b \\isin R$。那么@$\\frac{a^2+b^2}{2} \\ge ab$@，其中，该不等式取等号时，a @=@ b。基本不等式：如果$a, b \\isin R_+$。那么@$\\frac{a+b}{2} \\ge \\sqrt{ab}$@，其中，该不等式取等号时，a @=@ b。通常，我们将$\\frac{a+b}{2}$称为@算术平均数@，将$\\sqrt{ab}$称为@几何平均数@，所以，基本不等式可以表述为两个正数的@算术平均数@不小于@几何平均数@。",
+      },
+      {
+        "id": "8",
+        "type": "ShortAnswer",
+        "question": "请简要说明一下什么是机器学习？",
+        "lineofanswer": 5,
+        "answers": [
+          "机器学习是一种通过模拟人类智能来实现任务的人工智能。它可以进行视觉识别、语音识别、语言理解和决策等任务。人工智能的发展已经取得了显著的进展，未来有望在各个领域发挥重要作用。"
+        ]
+      },
+      {
+        "id": "9",
+        "type": "TrueFalse",
+        "question": "机器学习是人工智能的一个子集。",
+        "answers": [
+          "True"
+        ]
+      },
+      {
+        "id": "10",
+        "type": "Essay",
+        "question": "请写一篇关于人工智能的议论文",
+        "lineofanswer": 50,
+        "answers": [
+          "人工智能是一种通过模拟人类智能来实现任务的人工智能。它可以进行视觉识别、语音识别、语言理解和决策等任务。人工智能的发展已经取得了显著的进展，未来有望在各个领域发挥重要作用。"
+        ]
+      }
     ]
   };
 
@@ -94,9 +130,20 @@ export class AppComponent implements OnInit, AfterViewInit {
 
     this.print_answer_container!.nativeElement.appendChild(this.createAnswerSection());
 
-    // setTimeout(() => {
-    //   window.print();
-    // }, 500);
+    let uselatex = this.sampleData.items!.some((item) => {
+      return item.latex === true;
+    });
+    if (uselatex) {
+      renderMathInElement(this.print_container.nativeElement, {
+        delimiters: [
+          { left: '$$', right: '$$', display: true },
+          { left: '$', right: '$', display: false },
+          // { left: '\\(', right: '\\)', display: false },
+          // { left: '\\[', right: '\\]', display: true }
+        ],
+        throwOnError: false
+      });
+    }
   }
 
   private createAnswerSection() {
@@ -107,10 +154,20 @@ export class AppComponent implements OnInit, AfterViewInit {
     this.sampleData.items!.forEach((question, index) => {
       const answerDiv = this.render.createElement('div');
       answerDiv.className = 'answer-item';
-      answerDiv.innerHTML = `
-        <strong>第${index + 1}题</strong>（${this.getTypeName(question.type)}）：
-        ${this.formatAnswer(question)}
-      `;
+      if (question.type === QuestionBankTypeEnum.FillInTheBlank) {
+
+        let subcontents = question.question?.split('@');
+        let answers: string[] = [];
+        subcontents.forEach((sstr, idx) => {
+          if (idx % 2 === 0) {
+          } else {
+            answers.push(sstr);
+          }
+        });
+        answerDiv.innerHTML = `<strong>第${index + 1}题</strong>（${this.getTypeName(question.type)}）：${answers.join(' ')}`;
+      } else {
+        answerDiv.innerHTML = `<strong>第${index + 1}题</strong>（${this.getTypeName(question.type)}）：${this.formatAnswer(question)}`;
+      }
       answerSection.appendChild(answerDiv);
     });
 
@@ -122,9 +179,17 @@ export class AppComponent implements OnInit, AfterViewInit {
       case QuestionBankTypeEnum.SingleChoice:
       case QuestionBankTypeEnum.MultipleChoice:
         return `答案：${item.answers!.join(', ')}`;
+      case QuestionBankTypeEnum.TrueFalse:
+        return `答案：${item.answers![0] === 'True' ? '正确' : '错误'}`;
+      case QuestionBankTypeEnum.Dictation:
+        return `答案：${item.question}`;
       case QuestionBankTypeEnum.FillInTheBlank:
         return item.answers ? `答案：${item.answers!.join(', ')}` : '';
       case QuestionBankTypeEnum.ShortAnswer:
+      case QuestionBankTypeEnum.Essay:
+        if (item.answers && item.answers.length > 0) {
+          return `答案：${item.answers.join(' ')}`;
+        }
         return '略';
       default:
         return '';
@@ -139,8 +204,50 @@ export class AppComponent implements OnInit, AfterViewInit {
       [QuestionBankTypeEnum.Essay]: '作文',
       [QuestionBankTypeEnum.ShortAnswer]: '简答',
       [QuestionBankTypeEnum.TrueFalse]: '判断',
+      [QuestionBankTypeEnum.Dictation]: '默写',
     };
     return typeMap[type] || '未知题型';
+  }
+
+  private createQuestionDictationLine(strline: string, lineDiv: HTMLElement) {
+    let nprv = 0;
+    for (let i = 0; i < strline.length; i++) {
+      if (strline[i] === '，' ||
+        strline[i] === '。' ||
+        strline[i] === '；' ||
+        strline[i] === '！' ||
+        strline[i] === '：' ||
+        strline[i] === '？') {
+        let orgval = strline.slice(nprv, i);
+
+        const subinput = this.render.createElement('input');
+        subinput.className = 'print-input';
+        subinput.type = 'text';
+        subinput.style.width = `${orgval.length * 16}px`;
+        lineDiv.appendChild(subinput);
+        const label = this.render.createElement('span');
+        label.textContent = strline[i];
+        lineDiv.appendChild(label);
+
+        nprv = i + 1;
+      }
+    }
+  }
+  private createQuestionFillInTheBlankLine(strline: string, lineDiv: HTMLElement) {
+    let subcontents = strline.split('@');
+    subcontents.forEach((scontr, idx) => {
+      if (idx % 2 === 0) {
+        const label = this.render.createElement('span');
+        label.textContent = scontr;
+        lineDiv.appendChild(label);
+      } else {
+        const subinput = this.render.createElement('input');
+        subinput.className = 'print-input';
+        subinput.type = 'text';
+        subinput.style.width = `${scontr.length * 16}px`;
+        lineDiv.appendChild(subinput);
+      }
+    });
   }
 
   private createQuestionElement(questionData: QuestionBankItem, index: number) {
@@ -152,21 +259,33 @@ export class AppComponent implements OnInit, AfterViewInit {
     const spanLabel = this.render.createElement('span');
     spanLabel.textContent = `${index + 1}. 【${this.getTypeName(questionData.type)}】`;
     typeLabel.appendChild(spanLabel);
-    if (questionData.type === QuestionBankTypeEnum.FillInTheBlank) {
-      let subcontents = questionData.question?.split('@');
-      subcontents.forEach((sstr, idx) => {
-        if (idx % 2 === 0) {
-          const label = this.render.createElement('span');
-          label.textContent = sstr;
-          typeLabel.appendChild(label);
-        } else {
-          const subinput = this.render.createElement('input');
-          subinput.className = 'print-input';
-          subinput.type = 'text';
-          subinput.style.width = `${sstr.length * 16}px`;
-          typeLabel.appendChild(subinput);
-        }
-      });
+
+    if (questionData.type === QuestionBankTypeEnum.Dictation) {
+      const label = this.render.createElement('span');
+      label.textContent = questionData.title;
+      typeLabel.appendChild(label);
+
+      let sublines = questionData.question?.split('\n');
+      if(sublines.length === 1) {
+        this.createQuestionDictationLine(sublines[0], label);
+      } else {
+        sublines.forEach((sstr, idx) => {
+          const linediv = this.render.createElement('div');          
+          this.createQuestionDictationLine(sstr, linediv);
+          typeLabel.appendChild(linediv);
+        });
+      }
+    } else if (questionData.type === QuestionBankTypeEnum.FillInTheBlank) {
+      let sublines = questionData.question?.split('\n');
+      if (sublines.length === 1) {
+        this.createQuestionFillInTheBlankLine(sublines[0], typeLabel);
+      } else {
+        sublines.forEach((sstr, idx) => {
+          const linediv = this.render.createElement('div');
+          this.createQuestionFillInTheBlankLine(sstr, linediv);
+          typeLabel.appendChild(linediv); 
+        });
+      }
     } else if (questionData.type === QuestionBankTypeEnum.SingleChoice || questionData.type === QuestionBankTypeEnum.MultipleChoice) {
       const questionTitle = this.render.createElement('span');
       questionTitle.className = 'question-title';
@@ -177,7 +296,18 @@ export class AppComponent implements OnInit, AfterViewInit {
       answerArea.type = 'text';
       answerArea.className = 'print-input';
       answerArea.style.width = '100px';
+
       typeLabel.appendChild(answerArea);
+    } else if(questionData.type === QuestionBankTypeEnum.TrueFalse) {
+      const linediv = this.render.createElement('div');
+      const checkbox = this.render.createElement('input');
+      checkbox.type = 'checkbox';
+      linediv.appendChild(checkbox);
+      const label = this.render.createElement('span');
+      label.textContent = questionData.question;
+      linediv.appendChild(label);
+
+      typeLabel.appendChild(linediv); 
     } else {
       const questionTitle = this.render.createElement('span');
       questionTitle.className = 'question-title';
@@ -200,16 +330,36 @@ export class AppComponent implements OnInit, AfterViewInit {
       });
       questionDiv.appendChild(optionsList);
     } else if (questionData.type === QuestionBankTypeEnum.FillInTheBlank) {
-    } else if (questionData.type === QuestionBankTypeEnum.Essay) {
-      // 处理作文题
-      const requirements = this.render.createElement('div');
-      requirements.textContent = `要求：${questionData.question}`;
-      questionDiv.appendChild(requirements);
+      // No options for FillInTheBlank
+    } else if (questionData.type === QuestionBankTypeEnum.TrueFalse) {
+      // No options for TrueFalse
+    } else if (questionData.type === QuestionBankTypeEnum.ShortAnswer || questionData.type === QuestionBankTypeEnum.Essay) {
+      // No options for ShortAnswer, but it need save spaces for user input
+      if (questionData.lineofanswer) {
+        const answerBox = this.render.createElement('div');
+        for (let i = 0; i < questionData.lineofanswer; i++) {
+          const lineDiv = this.render.createElement('div');
+          lineDiv.className = 'print-multiple-input';
+          const input = this.render.createElement('input');
+          input.type = 'text';
+          input.className = 'print-input';
+          input.style.width = '100%';
+          lineDiv.appendChild(input);
+          answerBox.appendChild(lineDiv);
+        }
 
-      const essayBox = this.render.createElement('div');
-      essayBox.className = 'essay-grid';
-      essayBox.setAttribute('contenteditable', 'true');
-      questionDiv.appendChild(essayBox);
+        questionDiv.appendChild(answerBox);
+      }
+    // } else if (questionData.type === QuestionBankTypeEnum.Essay) {
+    //   // 处理作文题
+    //   const requirements = this.render.createElement('div');
+    //   requirements.textContent = `要求：${questionData.question}`;
+    //   questionDiv.appendChild(requirements);
+
+    //   const essayBox = this.render.createElement('div');
+    //   essayBox.className = 'essay-grid';
+    //   essayBox.setAttribute('contenteditable', 'true');
+    //   questionDiv.appendChild(essayBox);
     }
 
     return questionDiv;
--- src/app/models/questionbank.ts
+++ src/app/models/questionbank.ts
@@ -3,6 +3,7 @@ export enum QuestionBankTypeEnum {
     TrueFalse = 'TrueFalse',
     SingleChoice = 'SingleChoice',
     MultipleChoice = 'MultipleChoice',
+    Dictation = 'Dictation',
     FillInTheBlank = 'FillInTheBlank',
     ShortAnswer = 'ShortAnswer',
     Essay = 'Essay',
@@ -63,12 +64,30 @@ export type QuestionBankItemOption = Partial<Record<ValidOptionKeys, string>>;
 
 export interface QuestionBankItem {
     id: string;
+    // Type of the item.
     type: QuestionBankTypeKeys;
+    // Title, only available for Dictation.
+    title?: string;
+    // Question text.
+    // For Dictation, the question will be splited by the following characters: ；。？！：
+    // For FillInTheBlank, the answer is surrounded with character '@' in the question text.
+    // Specially, the `\n` will split the question text into multiple lines.
     question: string;
+    // Question text format. Default is Text.
     optionsFormat?: QuestionBankContentFormatKeys;
+    // If latex is true, the question text will be rendered as LaTeX.
     latex?: boolean;
+    // Options. Available for MultipleChoice and SingleChoice.
     options?: QuestionBankItemOption;
+    // Answer: available for all types except FillInTheBlank and Dictation.
+    // For Dictation and FillInTheBlank, the answer is in the question text.
+    // For MultipleChoice, the answer is an array of option keys.
+    // For SingleChoice, the answer is only option key.
+    // For TrueFalse, the answer is true or false.
+    // For Essay and ShortAnswer, the answer either be the correct answer or the reference.
     answers?: string[];
+    // Line of answer: available for Essay and ShortAnswer
+    lineofanswer?: number;
 }
 
 export interface QuestionBank {
